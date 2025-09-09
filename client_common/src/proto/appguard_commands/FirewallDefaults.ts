f688139d2e4a3fc72c9f0b891de6e6202054708b
GyulyVGC <gyulyvgc99@gmail.com>
2025-09-09 17:00:06 +0200

    update protobuf

--- client_common/src/proto/appguard_commands/FirewallDefaults.ts
+++ client_common/src/proto/appguard_commands/FirewallDefaults.ts
@@ -5,9 +5,11 @@ import type { FirewallPolicy as _appguard_commands_FirewallPolicy } from '../app
 export interface FirewallDefaults {
   'timeout'?: (number);
   'policy'?: (_appguard_commands_FirewallPolicy | keyof typeof _appguard_commands_FirewallPolicy);
+  'cache'?: (boolean);
 }
 
 export interface FirewallDefaults__Output {
   'timeout'?: (number);
   'policy'?: (_appguard_commands_FirewallPolicy);
+  'cache'?: (boolean);
 }
